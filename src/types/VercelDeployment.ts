750ddc29e71750205baf34e4cdbb43bdf4d0ac70
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-12-14 21:38:10 +0100

    Add proper TS type for VercelDeployment

--- src/types/VercelDeployment.ts
+++ src/types/VercelDeployment.ts
@@ -1,7 +1,28 @@
 /**
  * Vercel deployment shape returned by the Vercel API.
+ *
+ * @see https://vercel.com/docs/api#endpoints/deployments/create-a-new-deployment/response-parameters
  */
 export type VercelDeployment = {
   id?: string;
-  readyState?: string;
+  url?: string;
+  name?: string;
+  meta?: { [key: string]: string };
+  regions?: string[];
+  routes?: { src: string; dest: string }[];
+  functions?: { [functionPath: string]: { memory: number } };
+  plan?: string;
+  public?: boolean;
+  ownerId: string;
+  readyState?: 'INITIALIZING' | 'ANALYZING' | 'BUILDING' | 'DEPLOYING' | 'READY' | 'ERROR';
+  createdAt?: Date;
+  createdIn?: string;
+  env?: string[];
+  build?: {
+    env?: string[];
+  };
+  target?: 'staging' | 'production';
+  alias?: string[];
+  aliasError?: { code: string; message: string };
+  aliasAssigned?: boolean;
 }
